eaa3f299c07c152eb8e0f95ae66bae8a32676ff6
man1230 <marcusnapoleoni@knights.ucf.edu>
2021-08-31 22:17:11 -0400

    Zork 1.3 update
    Fixed switch cases so that directions are grouped

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -26,25 +26,16 @@ namespace Zork
                         break;
 
                     case Commands.NORTH:
-                        outputString = "You moved NORTH.";
-                        break;
-
                     case Commands.SOUTH:
-                        outputString = "You moved SOUTH.";
-                        break;
-
                     case Commands.EAST:
-                        outputString = "You moved EAST.";
-                        break;
-
                     case Commands.WEST:
-                        outputString = "You moved WEST.";
+                        outputString = $"You moved {command}.";
                         break;
 
                     default:
                         outputString = "Unknown command";
                         break;
-                };
+                }
 
                 Console.WriteLine(outputString);
             }
