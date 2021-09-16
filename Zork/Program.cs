fc8e88338749486f8100138b024551f1dec423de
man1230 <marcusnapoleoni@knights.ucf.edu>
2021-09-15 21:28:10 -0400

    Zork 2.2

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -8,7 +8,7 @@ namespace Zork
         {
             get
             {
-                return Rooms[LocationColumn];
+                return Rooms[LocationRow, LocationColumn];
             }
         }
 
@@ -16,37 +16,36 @@ namespace Zork
         {
             Console.WriteLine("Welcome to Zork!");
 
-            while (true)
+            Commands command = Commands.UNKNOWN;
+            while (command != Commands.QUIT)
             {
                 Console.Write($"{Location}\n> ");
-                Commands command = ToCommand(Console.ReadLine().Trim());
-                if (command == Commands.QUIT)
-                    break;
+                command = ToCommand(Console.ReadLine().Trim());
 
-                string outputString;
                 switch (command)
                 {
                     case Commands.QUIT:
-                        outputString = "Thank you for playing";
+                        Console.WriteLine("Thank you for playing");
                         break;
 
                     case Commands.LOOK:
-                        outputString = "This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        Console.WriteLine("This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.");
                         break;
 
                     case Commands.NORTH:
                     case Commands.SOUTH:
                     case Commands.EAST:
                     case Commands.WEST:
-                        outputString = (Move(command)) ? $"You Moved {command}." : "The way is shut";
+                        if ((Move(command) == false))
+                        {
+                            Console.WriteLine("The way is shut");
+                        }
                         break;
 
                     default:
-                        outputString = "Unknown command";
+                        Console.WriteLine("Unknown command");
                         break;
                 }
-
-                Console.WriteLine(outputString);
             }
         }
 
@@ -56,22 +55,21 @@ namespace Zork
 
             switch (command)
             {
-                case Commands.NORTH:
-                case Commands.SOUTH:
+                case Commands.NORTH when LocationRow < Rooms.GetLength(0) - 1:
+                    LocationRow++;
+                    didMove = true;
                     break;
-                case Commands.EAST:
-                    if (LocationColumn < Rooms.Length - 1)
-                    {
-                        LocationColumn++;
-                        didMove = true;
-                    }
+                case Commands.SOUTH when LocationRow > 0:
+                    LocationRow--;
+                    didMove = true;
                     break;
-                case Commands.WEST:
-                    if (LocationColumn > 0)
-                    {
-                        LocationColumn--;
-                        didMove = true;
-                    }
+                case Commands.EAST when LocationColumn < Rooms.GetLength(1) - 1:
+                    LocationColumn++;
+                    didMove = true;
+                    break;
+                case Commands.WEST when LocationColumn > 0:
+                    LocationColumn--;
+                    didMove = true;
                     break;
             }
             return didMove;
@@ -79,7 +77,14 @@ namespace Zork
 
         private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
 
-        private static string[] Rooms = { "Forest", "West of House", "Behind House", "Clearing", "Canyon View" };
+        private static string[,] Rooms =
+        {
+            { "Rocky Trail", "South of House", "Canyon View" },
+            { "Forest", "West of House", "Behind House" },
+            { "Dense Woods", "North of House", "Clearing" }
+        };
+
+        private static int LocationRow = 1;
         private static int LocationColumn = 1;
     }
 }
